300a97ea79e2d8657c0ca7b9d3de27cb8a916e32
wuguizaibowodeke <wuguizaibowodeke@gmail.com>
2025-05-11 00:12:11 +0800

    添加随机数生成类

--- source/include/util/algorithm.hpp
+++ source/include/util/algorithm.hpp
@@ -1,2 +1,32 @@
 #pragma once
 
+#include <random>
+/**
+ * @brief 生成随机数
+ */
+class RandomNumberGenerator
+{
+public:
+    RandomNumberGenerator(size_t seed)
+    {
+        setSeed(seed);
+    }
+
+    RandomNumberGenerator() : RandomNumberGenerator(0)
+    {
+    }
+
+    void setSeed(size_t seed)
+    {
+        m_generator.seed(seed);
+    }
+
+    float uniform()
+    {
+        return m_distribution(m_generator);
+    }
+
+private:
+    std::mt19937 m_generator;
+    std::uniform_real_distribution<float> m_distribution{0, 1};
+};
