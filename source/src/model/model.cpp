921fccc84f7385c695456d47ce77682621a61ff8
wuguizaibowodeke <wuguizaibowodeke@gmail.com>
2025-05-14 22:03:04 +0800

    修改模型从文件读取方式

--- source/src/model/model.cpp
+++ source/src/model/model.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include "util/profile.hpp"
+#include <rapidobj/rapidobj.hpp>
 
 Model::Model(const std::vector<Triangle> &triangles)
     : m_triangles(triangles)
@@ -13,52 +14,59 @@ Model::Model(const std::vector<Triangle> &triangles)
 Model::Model(const std::filesystem::path &filename)
 {
     PROFILE("Load model" + filename.string())
-    std::vector<glm::vec3> positions;
-    std::vector<glm::vec3> normals;
 
-    std::ifstream file(filename);
-    if (!file.is_open())
+    auto result = rapidobj::ParseFile(filename, rapidobj::MaterialLibrary::Ignore());
+
+    for (const auto &shape : result.shapes)
     {
-        std::cerr << "Failed to open model file: " << filename << std::endl;
-        return;
-    }
-
-    std::string line = "";
-    char trash;
-    while (!file.eof())
-    {
-        std::getline(file, line);
-        std::istringstream iss(line);
-
-        if (line.compare(0, 2, "v ") == 0) // 顶点坐标
+        size_t index_offset = 0;
+        for (size_t num_face_vectex : shape.mesh.num_face_vertices)
         {
-            glm::vec3 position;
-            iss >> trash >> position.x >> position.y >> position.z;
-            positions.push_back(position);
-        }
+            if (num_face_vectex == 3)
+            {
+                auto index = shape.mesh.indices[index_offset];
+                glm::vec3 pos0{
+                    result.attributes.positions[index.position_index * 3 + 0],
+                    result.attributes.positions[index.position_index * 3 + 1],
+                    result.attributes.positions[index.position_index * 3 + 2]};
+                index = shape.mesh.indices[index_offset + 1];
+                glm::vec3 pos1{
+                    result.attributes.positions[index.position_index * 3 + 0],
+                    result.attributes.positions[index.position_index * 3 + 1],
+                    result.attributes.positions[index.position_index * 3 + 2]};
+                index = shape.mesh.indices[index_offset + 2];
+                glm::vec3 pos2{
+                    result.attributes.positions[index.position_index * 3 + 0],
+                    result.attributes.positions[index.position_index * 3 + 1],
+                    result.attributes.positions[index.position_index * 3 + 2]};
 
-        if (line.compare(0, 3, "vn ") == 0) // 顶点法向量
-        {
-            glm::vec3 normal;
-            iss >> trash >> trash >> normal.x >> normal.y >> normal.z;
-            normals.push_back(normal);
-        }
-
-        if (line.compare(0, 2, "f ") == 0) // 三角面
-        {
-            glm::ivec3 idx_v, idx_vn;
-            iss >> trash;
-            iss >> idx_v.x >> trash >> trash >> idx_vn.x;
-            iss >> idx_v.y >> trash >> trash >> idx_vn.y;
-            iss >> idx_v.z >> trash >> trash >> idx_vn.z;
-
-            m_triangles.push_back(Triangle(
-                positions[idx_v.x - 1],
-                positions[idx_v.y - 1],
-                positions[idx_v.z - 1],
-                normals[idx_vn.x - 1],
-                normals[idx_vn.y - 1],
-                normals[idx_vn.z - 1]));
+                if (index.normal_index >= 0)
+                {
+                    index = shape.mesh.indices[index_offset];
+                    glm::vec3 normal0{
+                        result.attributes.normals[index.normal_index * 3 + 0],
+                        result.attributes.normals[index.normal_index * 3 + 1],
+                        result.attributes.normals[index.normal_index * 3 + 2]};
+                    index = shape.mesh.indices[index_offset + 1];
+                    glm::vec3 normal1{
+                        result.attributes.normals[index.normal_index * 3 + 0],
+                        result.attributes.normals[index.normal_index * 3 + 1],
+                        result.attributes.normals[index.normal_index * 3 + 2]};
+                    index = shape.mesh.indices[index_offset + 2];
+                    glm::vec3 normal2{
+                        result.attributes.normals[index.normal_index * 3 + 0],
+                        result.attributes.normals[index.normal_index * 3 + 1],
+                        result.attributes.normals[index.normal_index * 3 + 2]};
+                    m_triangles.push_back(Triangle{
+                        pos0, pos1, pos2, normal0, normal1, normal2});
+                }
+                else
+                {
+                    m_triangles.push_back(Triangle{
+                        pos0, pos1, pos2});
+                }
+            }
+            index_offset += num_face_vectex;
         }
     }
 
@@ -88,7 +96,7 @@ std::optional<RayHitInfo> Model::intersect(const Ray &ray, float t_min, float t_
 
 void Model::calculateBound()
 {
-    for(const auto &triangle : m_triangles)
+    for (const auto &triangle : m_triangles)
     {
         m_bound.expand(triangle.getVertex(0));
         m_bound.expand(triangle.getVertex(1));
